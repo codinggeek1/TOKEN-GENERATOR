7aea59f890deb8be47767094ce208f56109cc8dd
PARDHIV VARMA <118751512+codinggeek1@users.noreply.github.com>
2023-07-18 19:58:49 +0530

    Update DashboardComp.jsx

--- components/DashboardComp.jsx
+++ components/DashboardComp.jsx
@@ -7,7 +7,7 @@ import MintModal from './modals/MintModal'
 import { useWeb3React } from '@web3-react/core'
 
 export default function DashboardComp({ tokenAddress }) {
-  const web3 = new Web3('https://polygon-mumbai-bor.publicnode.com')
+  const web3 = new Web3('https://rpc-mumbai.maticvigil.com')
   const contractAddress = tokenAddress
 
   const [tokenName, setTokenName] = useState()
@@ -18,7 +18,7 @@ export default function DashboardComp({ tokenAddress }) {
   const [isChangeMinter, setIsChangeMinter] = useState(false)
 
   let explorerLink =
-    'https://polygonscan.com/' + tokenAddress
+    'https://mumbai.polygonscan.com/' + tokenAddress
 
   const ERC20TokenContract = new web3.eth.Contract(ERC20ABI, contractAddress)
 
